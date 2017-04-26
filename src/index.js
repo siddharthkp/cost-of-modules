79b336dfa0ab13a0cd8c84e0e650869521107fb8
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-04-26 12:50:39 +0530

    first time prettify

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const helpers = require('./helpers');
+const helpers = require("./helpers");
 
 helpers.setup();
 
-console.log('Calculating...');
+console.log("Calculating...");
 console.log();
 
 /*
@@ -37,15 +37,17 @@ let flatDependencies = helpers.attachNestedDependencies(rootDependencies);
     Modules actual size = size of the module + size of it's children
 */
 for (let i = 0; i < flatDependencies.length; i++) {
-    let dep = flatDependencies[i];
+  let dep = flatDependencies[i];
 
-    let sizeOfModule = moduleSizes[dep.name];
+  let sizeOfModule = moduleSizes[dep.name];
 
-    let sizeOfChildren = 0;
-    dep.children.forEach((child) => { sizeOfChildren += moduleSizes[child] || 0; });
+  let sizeOfChildren = 0;
+  dep.children.forEach(child => {
+    sizeOfChildren += moduleSizes[child] || 0;
+  });
 
-    dep.actualSize = sizeOfModule + sizeOfChildren;
-    dep.numberOfChildren = dep.children.length;
+  dep.actualSize = sizeOfModule + sizeOfChildren;
+  dep.numberOfChildren = dep.children.length;
 }
 
 /*
@@ -57,7 +59,9 @@ let allDependencies = helpers.getAllDependencies(flatDependencies);
 
 /* Total size of node_modules */
 let totalSize = 0;
-allDependencies.forEach((dep) => { totalSize += moduleSizes[dep] || 0; });
+allDependencies.forEach(dep => {
+  totalSize += moduleSizes[dep] || 0;
+});
 
 /* Display results */
 helpers.displayResults(flatDependencies, allDependencies, totalSize);
