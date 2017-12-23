5590520080d23f5b26856ddc2c4b242ffabea668
oriash93 <oriash93@gmail.com>
2017-12-23 11:34:57 +0200

    Fixed test data files

--- test/src/fixtures/dev-test-data.js
+++ test/src/fixtures/dev-test-data.js
@@ -1,40 +1,40 @@
 let moduleSizesArray = [
-  {name: '@siddharthkp/empty', size: 1},
-  {name: 'camelcase', size: 5},
-  {name: 'date-time', size: 4},
-  {name: 'once', size: 5},
-  {name: 'time-zone', size: 4},
-  {name: 'wrappy', size: 4},
-  {name: 'yargs-parser', size: 35}
-];
+  { name: '@siddharthkp/empty', size: 0 },
+  { name: 'camelcase', size: 4 },
+  { name: 'date-time', size: 3 },
+  { name: 'once', size: 4 },
+  { name: 'time-zone', size: 3 },
+  { name: 'wrappy', size: 3 },
+  { name: 'yargs-parser', size: 34 },
+]
 
 let rootDependencies = [
-    '@siddharthkp/empty',
-    'date-time',
-    'once',
-    'yargs-parser'
-];
+  '@siddharthkp/empty',
+  'date-time',
+  'once',
+  'yargs-parser',
+]
 
 let flatDependencies = [
-    {name: '@siddharthkp/empty',  children:[]},
-    {name: 'date-time', children: ['time-zone']},
-    {name: 'once', children: ['wrappy']},
-    {name: 'yargs-parser', children: ['camelcase']}
-];
+  { name: '@siddharthkp/empty', children: [] },
+  { name: 'date-time', children: ['time-zone'] },
+  { name: 'once', children: ['wrappy'] },
+  { name: 'yargs-parser', children: ['camelcase'] },
+]
 
 let allDependencies = [
-    '@siddharthkp/empty',
-    'camelcase',
-    'date-time',
-    'once',
-    'time-zone',
-    'wrappy',
-    'yargs-parser'
-];
+  '@siddharthkp/empty',
+  'camelcase',
+  'date-time',
+  'once',
+  'time-zone',
+  'wrappy',
+  'yargs-parser',
+]
 
 module.exports = {
-    moduleSizesArray,
-    rootDependencies,
-    flatDependencies,
-    allDependencies
-};
+  moduleSizesArray,
+  rootDependencies,
+  flatDependencies,
+  allDependencies,
+}
--- test/src/fixtures/test-data.js
+++ test/src/fixtures/test-data.js
@@ -1,35 +1,32 @@
 let moduleSizesArray = [
-  {name: '@siddharthkp/empty', size: 1},
-  {name: 'date-time', size: 4},
-  {name: 'once', size: 5},
-  {name: 'time-zone', size: 4},
-  {name: 'wrappy', size: 4}
-];
+  { name: '@siddharthkp/empty', size: 0 },
+  { name: 'camelcase', size: 4 },
+  { name: 'date-time', size: 3 },
+  { name: 'once', size: 4 },
+  { name: 'time-zone', size: 3 },
+  { name: 'wrappy', size: 3 },
+  { name: 'yargs-parser', size: 34 },
+]
 
-let rootDependencies = [
-    '@siddharthkp/empty',
-    'date-time',
-    'once',
-
-];
+let rootDependencies = ['@siddharthkp/empty', 'date-time', 'once']
 
 let flatDependencies = [
-    {name: '@siddharthkp/empty',  children:[]},
-    {name: 'date-time', children:['time-zone']},
-    {name: 'once', children:['wrappy']}
-];
+  { name: '@siddharthkp/empty', children: [] },
+  { name: 'date-time', children: ['time-zone'] },
+  { name: 'once', children: ['wrappy'] },
+]
 
 let allDependencies = [
-    '@siddharthkp/empty',
-    'date-time',
-    'once',
-    'time-zone',
-    'wrappy'
-];
+  '@siddharthkp/empty',
+  'date-time',
+  'once',
+  'time-zone',
+  'wrappy',
+]
 
 module.exports = {
-    moduleSizesArray,
-    rootDependencies,
-    flatDependencies,
-    allDependencies
-};
+  moduleSizesArray,
+  rootDependencies,
+  flatDependencies,
+  allDependencies,
+}
